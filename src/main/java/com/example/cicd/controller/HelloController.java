d40c19c8d466927b32b520ec22a72f909330b36c
steamedEggMaster <cksdud5014@naver.com>
2024-06-02 20:38:37 +0900

    [master] hello

---
+++ src/main/java/com/example/cicd/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.cicd.controller;
+
+/**
+ * @PackageName : com.example.cicd.controller
+ * @FileName : HelloController
+ * @Author : noglass_gongdae
+ * @Date : 2024-06-02
+ * @Blog : https://blog.naver.com/noglass_gongdae
+ * @GitHub :
+ */
+public class HelloController {
+
+}
