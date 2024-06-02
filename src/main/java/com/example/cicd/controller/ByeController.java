eb6560a714ab53ea675a34a024ca2e2135d80016
steamedEggMaster <cksdud5014@naver.com>
2024-06-02 20:44:24 +0900

    [master] bye

---
+++ src/main/java/com/example/cicd/controller/ByeController.java
@@ -0,0 +1,13 @@
+package com.example.cicd.controller;
+
+/**
+ * @PackageName : com.example.cicd.controller
+ * @FileName : ByeController
+ * @Author : noglass_gongdae
+ * @Date : 2024-06-02
+ * @Blog : https://blog.naver.com/noglass_gongdae
+ * @GitHub :
+ */
+public class ByeController {
+
+}
