8c4edc1613155a9644d795cb3e76e5538f8cc742
steamedEggMaster <cksdud5014@naver.com>
2024-06-05 23:26:22 +0900

    [master] ha5

--- src/main/java/com/example/cicd/controller/HelloController.java
+++ src/main/java/com/example/cicd/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.cicd.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @PackageName : com.example.cicd.controller
  * @FileName : HelloController
@@ -8,6 +10,8 @@ package com.example.cicd.controller;
  * @Blog : https://blog.naver.com/noglass_gongdae
  * @GitHub :
  */
+
+@Controller
 public class HelloController {
 
 }
