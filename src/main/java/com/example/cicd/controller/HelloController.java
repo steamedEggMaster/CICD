bc601025cc289a1d40f046cefcd5811c891dfc1f
steamedEggMaster <cksdud5014@naver.com>
2024-06-25 17:45:07 +0900

    [master] 안녕하세요

--- src/main/java/com/example/cicd/controller/HelloController.java
+++ src/main/java/com/example/cicd/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.cicd.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @PackageName : com.example.cicd.controller
@@ -11,7 +13,12 @@ import org.springframework.stereotype.Controller;
  * @GitHub :
  */
 
-@Controller
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "안녕하세요";
+    }
 
 }
